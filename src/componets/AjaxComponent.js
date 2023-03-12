c566a2e931f6fb3f5333b2e582791b4961b906db
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-03-12 15:54:10 +0100

    request ajax async await

--- src/componets/AjaxComponent.js
+++ src/componets/AjaxComponent.js
@@ -2,49 +2,61 @@ import React, { useEffect, useState } from 'react'
 
 export const AjaxComponent = () => {
 
-const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
 //Generic / basic
-const getStaticUser = () => {
-  setUser([
-    {
-      id: 1,
-      email: "michael.lawson@reqres.in",
-      first_name: "Michael",
-      last_name: "Lawson",
-      avatar: "https://reqres.in/img/faces/7-image.jpg",
-    },
-    {
-      id: 2,
-      email: "lindsay.ferguson@reqres.in",
-      first_name: "Lindsay",
-      last_name: "Ferguson",
-      avatar: "https://reqres.in/img/faces/8-image.jpg",
-    },
-    {
-      id: 3,
-      email: "tobias.funke@reqres.in",
-      first_name: "Tobias",
-      last_name: "Funke",
-      avatar: "https://reqres.in/img/faces/9-image.jpg",
-    },
-  ]);
-};
+// const getStaticUser = () => {
+//   setUser([
+//     {
+//       id: 1,
+//       email: "michael.lawson@reqres.in",
+//       first_name: "Michael",
+//       last_name: "Lawson",
+//       avatar: "https://reqres.in/img/faces/7-image.jpg",
+//     },
+//     {
+//       id: 2,
+//       email: "lindsay.ferguson@reqres.in",
+//       first_name: "Lindsay",
+//       last_name: "Ferguson",
+//       avatar: "https://reqres.in/img/faces/8-image.jpg",
+//     },
+//     {
+//       id: 3,
+//       email: "tobias.funke@reqres.in",
+//       first_name: "Tobias",
+//       last_name: "Funke",
+//       avatar: "https://reqres.in/img/faces/9-image.jpg",
+//     },
+//   ]);
+// };
   
-  const getUserAjaxPms = () => {
-    fetch("https://reqres.in/api/users?page=2")
-      .then(response => response.json())
-      .then(final_result => {
-        setUser(final_result.data);
-        console.log(user);
-      },
-        error => {
-          console.log(error);
-      })
+  // const getUserAjaxPms = () => {
+  //   fetch("https://reqres.in/api/users?page=2")
+  //     .then(response => response.json())
+  //     .then(final_result => {
+  //       setUser(final_result.data);
+  //       console.log(user);
+  //     },
+  //       error => {
+  //         console.log(error);
+  //     })
+  // }
+
+
+  const getUserAjaxAW = async() => {
+    const request = await fetch("https://reqres.in/api/users?page=2");
+    const {data} = await request.json();
+
+    setUser(data);
   }
+
+
   
   useEffect(() => {
-    getUserAjaxPms();
+    //getStaticUser();
+    //getUserAjaxPms();
+    getUserAjaxAW();
   }, []);
 
 
